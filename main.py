d23d3130efbe3c9fe13ec5c1b3c2810fb8a76a20
WeronikaSzadzinska <65089636+WeronikaSzadzinska@users.noreply.github.com>
2020-09-10 18:24:08 -0500

    Add some additional features to main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-import tmdb_client
+from flask import Flask, render_template, url_for, request, redirect
+import tmdb_client, random, requests
 
 app=Flask(__name__)
 
@@ -7,10 +7,16 @@ app=Flask(__name__)
 def homepage():
     return render_template("index.html")"""
 
+
+
 @app.route('/')
 def homepage():
-    movies = tmdb_client.get_shuffle(how_many=6)
-    return render_template("homepage.html", movies=movies)
+    movie_list = ['popular', 'top_rated', 'upcoming', 'now_playing' ]
+    selected_list = request.args.get('list_type', "popular")
+    movies = tmdb_client.get_movies(how_many=8, list_type=selected_list)
+    if selected_list != 'popular' and  selected_list != 'top_rated' and  selected_list != 'now_playing' and  selected_list != 'upcoming' :
+       selected_list == 'popular' 
+    return render_template("homepage.html", movies=movies, current_list=selected_list, movie_list=movie_list)
 
 @app.context_processor
 def utility_processor():
@@ -18,5 +24,20 @@ def utility_processor():
         return tmdb_client.get_poster_url(path, size)
     return {"tmdb_image_url": tmdb_image_url}
 
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+    #random_cover = tmdb_client.get_random_cover(movie_id)
+    details = tmdb_client.get_single_movie(movie_id)
+    cast = tmdb_client.get_single_movie_cast(movie_id)
+    movie_images = tmdb_client.get_movie_images(movie_id)
+    selected_backdrop = random.choice(movie_images['backdrops'])
+    return render_template("movie_details.html", movie=details, cast=cast, selected_backdrop=selected_backdrop)
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template("homepage.html")
+
+
+
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
