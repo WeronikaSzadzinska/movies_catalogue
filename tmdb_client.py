89f347d68e4f22396670dfe0d5f9fb60d11745b8
WeronikaSzadzinska <65089636+WeronikaSzadzinska@users.noreply.github.com>
2020-09-11 11:22:09 -0500

    Change a bit the file function

--- tmdb_client.py
+++ tmdb_client.py
@@ -3,8 +3,7 @@ import requests, random
 API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OTZjMDM3ZDhkOWJjZDFjYWFmMDRjNWRhNTE0NTBiNiIsInN1YiI6IjVmNTQxMTQ0NjY1NDA4MDAzMTA5YTdiZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.N3v5u-yLnBRrsVY6Q7sMVtYmcKBO07IfoHN6RLKEZdE"
 
 def get_movies(how_many, list_type):
-    if list_type != 'popular' and  list_type != 'top_rated' and  list_type != 'now_playing' and  list_type != 'upcoming' :
-       list_type == 'popular'
+ 
     endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
     headers = {
         "Authorization": f"Bearer {API_TOKEN}"
@@ -51,4 +50,3 @@ def get_shuffle(how_many):
     data = get_movies()
     data_ran= random.sample(data['results'], how_many)
     return data_ran
-
